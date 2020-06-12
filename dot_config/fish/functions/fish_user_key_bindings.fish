44c2b026573b11f800e4df4fea077edd7957d6db
Liam Dawson <liam@ldaws.com>
2020-06-12 15:50:27 +1000

    Add git-logs from gitnow

--- dot_config/fish/functions/fish_user_key_bindings.fish
+++ dot_config/fish/functions/fish_user_key_bindings.fish
@@ -3,6 +3,8 @@ function fish_user_key_bindings -d "Vi-style key bindings, plus some extra fancy
 
   for mode in default insert
     # alt-g = short git summary
-    bind -M $mode \u00A9 'echo; git-state; echo; commandline -f repaint;'
+    bind -M $mode \u00A9 'echo; git-state; echo;'
+    # alt-l = fancy git log
+    bind -M $mode \u00AC 'echo; git-logs; echo;'
   end
 end
--- dot_config/fish/functions/git-state.fish
+++ dot_config/fish/functions/git-state.fish
@@ -1,5 +1,6 @@
 # https://github.com/joseluisq/gitnow/blob/ae2de93cfa863e370b84454210cc3618df5fab90/conf.d/gitnow.fish#L13-L18
 
 function git-state -d "Show the working tree status in compact way"
-  git status -sb
+  command git status -sb
+  commandline -f repaint;
 end
