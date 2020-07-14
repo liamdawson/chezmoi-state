d5665ddc23129ac77104d1e70ea7b78ed880a98e
Liam Dawson <liam@ldaws.com>
2020-07-14 11:01:01 +1000

    ghq cd bind

--- dot_config/fish/functions/fish_user_key_bindings.fish
+++ dot_config/fish/functions/fish_user_key_bindings.fish
@@ -3,8 +3,9 @@ function fish_user_key_bindings -d "Vi-style key bindings, plus some extra fancy
 
   for mode in default insert
     # alt-g = short git summary
-    bind -M $mode \u00A9 'echo; git-state; echo;'
+    bind -M $mode \u00A9 'echo; git-state; echo; commandline -f repaint'
     # alt-l = fancy git log
-    bind -M $mode \u00AC 'echo; git-logs; echo;'
+    bind -M $mode \u00AC 'echo; git-logs; echo; commandline -f repaint;'
+    bind -M $mode \c_ 'cd (ghq list -p 2>/dev/null | peco); and commandline -f repaint'
   end
 end
