e7508ffbd08c77ee36bc1067b2528935b516545d
Liam Dawson <liam@ldaws.com>
2020-07-30 11:59:33 +1000

    Add usage for ecr-local-login

--- dot_config/fish/functions/ecr-local-login.fish
+++ dot_config/fish/functions/ecr-local-login.fish
@@ -1,3 +1,8 @@
 function ecr-local-login --argument-names 'profile' 'account' 'region'
+  if [ (count $argv) -lt 3 ]
+    echo "usage: ecr-local-login <profile> <account> <region>"
+    return 1
+  end
+
   aws-vault exec "$profile" -- aws ecr get-login-password --region="$region" | docker login --username AWS --password-stdin "$account.dkr.ecr.$region.amazonaws.com"
 end
