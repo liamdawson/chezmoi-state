44d625d3b1dc7433096816779064899ee74357f5
Liam Dawson <liam@ldaws.com>
2020-09-08 12:50:48 +1000

    Add direnv/bundle exec abbreviations

--- dot_config/fish/conf.d/abbreviations.fish
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -2,5 +2,7 @@ if status --is-interactive
   if type -q aws-vault
     abbr --global --add avl "aws-vault login"
     abbr --global --add ave "aws-vault exec"
+    abbr --global --add dx "direnv exec ."
+    abbr --global --add bx "bundle exec"
   end
 end
