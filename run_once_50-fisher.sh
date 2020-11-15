78eefb551edc36424e4b8f2b0f2d94cd4e424420
Liam Dawson <liam@ldaws.com>
2020-11-16 00:36:53 +1100

    Fix run_once issues

--- run_once_50-fisher.sh
+++ run_once_50-fisher.sh
@@ -7,7 +7,7 @@ if command -v fish >/dev/null 2>&1; then
   if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+    fish -c 'fisher update'
   end
 EOS
 fi
--- run_once_80-rustup.sh
+++ run_once_80-rustup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 set -Eeuo pipefail
+# can end up with rust on path through homebrew
+export RUSTUP_INIT_SKIP_PATH_CHECK="yes"
 
 if ! command -v rustup >/dev/null 2>&1; then
   curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
