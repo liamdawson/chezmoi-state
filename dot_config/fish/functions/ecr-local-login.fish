d30e6e86d2247f2006efb8dccef24ec6979abb52
Liam Dawson <liam@ldaws.com>
2021-03-25 10:44:56 +1100

    Echo cmd for ecr-local-login

--- dot_config/fish/functions/ecr-local-login.fish
+++ dot_config/fish/functions/ecr-local-login.fish
@@ -4,5 +4,6 @@ function ecr-local-login --argument-names 'profile' 'account' 'region'
     return 1
   end
 
+  echo "aws-vault exec '$profile' -- aws ecr get-login-password --region '$region' | docker login --username AWS --password-stdin '$account.dkr.ecr.$region.amazonaws.com'"
   aws-vault exec "$profile" -- aws ecr get-login-password --region="$region" | docker login --username AWS --password-stdin "$account.dkr.ecr.$region.amazonaws.com"
 end
