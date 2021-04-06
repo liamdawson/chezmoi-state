f454efad7c2f994f60899cb1411042069174303c
Liam Dawson <liam@ldaws.com>
2021-04-06 14:33:15 +1000

    Add raycast aws-console script

---
+++ private_dot_local/private_share/raycast/scripts/mine/executable_aws-console.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title AWS Console
+# @raycast.mode compact
+#
+# Optional parameters:
+# @raycast.icon 🔐
+# @raycast.currentDirectoryPath ~
+# @raycast.packageName Work
+
+set -Eeuo pipefail
+
+profile="$(aws-vault list --profiles | choose)"
+login_url="$(aws-vault login "$profile" --stdout)"
+encoded_url="${login_url//&/%26}"
+firefox_uri="ext+container:name=${profile}&url=${encoded_url}"
+
+/Applications/Firefox.app/Contents/MacOS/firefox "$firefox_uri"
