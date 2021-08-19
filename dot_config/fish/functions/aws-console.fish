19f0aa1d9a03f5160b1d7918be72635c65cf93bc
Liam Dawson <liam@ldaws.com>
2021-08-19 13:06:34 +1000

    Allow extra args through aws-console

--- dot_config/fish/functions/aws-console.fish
+++ dot_config/fish/functions/aws-console.fish
@@ -1,6 +1,9 @@
 if type -q aws-vault
   function aws-console --argument-names 'profile' --description "Open the AWS Console (for an aws-vault profile) in a Firefox Container tab"
-    set -l login_url (aws-vault login "$profile" --stdout) || return $status
+    set -e argv[1]
+    set -l leftover_args $argv
+
+    set -l login_url (aws-vault login "$profile" --stdout $leftover_args) || return $status
     set -l encoded_url (string replace --all '&' '%26' "$login_url")
     set -l firefox_uri "ext+container:name=$profile&url=$encoded_url"
 
