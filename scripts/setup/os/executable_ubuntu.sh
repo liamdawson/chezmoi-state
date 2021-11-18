cd406a1cddacde62b2e738b3875b71b612525d66
Liam Dawson <liam@ldaws.com>
2021-11-18 20:08:44 +1100

    Change Ubuntu emoji shortcut

--- scripts/setup/os/executable_ubuntu.sh
+++ scripts/setup/os/executable_ubuntu.sh
@@ -8,13 +8,6 @@ main() {
   tilix_config
 }
 
-configure_themes() {
-  ghq get dracula/tilix
-
-  mkdir -p "${HOME}/.config/tilix/schemes"
-  cp "$(ghq list --exact --full-path dracula/tilix)/Dracula.json" "${HOME}/.config/tilix/schemes/"
-}
-
 gnome_ui_settings() {
   # capslock => escape
   dconf write /org/gnome/desktop/input-sources/xkb-options "['terminate:ctrl_alt_bksp', 'caps:escape']"
@@ -22,6 +15,8 @@ gnome_ui_settings() {
   dconf write /org/gnome/desktop/interface/show-battery-percentage true
   # touchpad scroll direction
   dconf write /org/gnome/desktop/peripherals/touchpad/natural-scroll false
+
+  gsettings set org.freedesktop.ibus.panel.emoji hotkey "['<Super>period']"
 }
 
 tilix_config() {
@@ -47,30 +42,6 @@ use-system-font=false
 use-theme-colors=false
 visible-name='Default'
 EOF
-
 }
 
 main
-
-cat >/dev/null <<EOF
-# Dracula Tilix colours
-
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/use-theme-colors
-  false
-
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/background-color
-  '#282936'
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/badge-color-set
-  false
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/bold-color-set
-  false
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/cursor-colors-set
-  false
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/foreground-color
-  '#F8F8F2'
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/highlight-colors-set
-  false
-
-/com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/palette
-  ['#000000', '#FF5454', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF79C6', '#8BE8FD', '#BFBFBF', '#4D4D4D', '#FF6E67', '#5AF78D', '#F4F99D', '#CAA8FA', '#FF92D0', '#9AEDFE', '#E6E6E6']
-EOF
