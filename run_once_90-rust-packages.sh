4f68119910c39a01b92e70c6346d014b6fd3b4df
Liam Dawson <liam@ldaws.com>
2021-11-26 18:25:45 +1100

    Handle cargo not being on path yet

--- run_once_90-rust-packages.sh
+++ run_once_90-rust-packages.sh
@@ -12,6 +12,12 @@ crates=(
   jwt-cli
 )
 
+if ! command -v cargo >/dev/null 2>&1; then
+  if test -x ~/.cargo/bin/cargo; then
+    export PATH="$HOME/.cargo/bin:$PATH"
+  fi
+fi
+
 for crate in "${crates[@]}"; do
   cargo install "$crate"
 done
