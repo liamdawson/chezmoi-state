254669b36b0d31bc047049c8ef9a170a1bce9dc4
Liam Dawson <liam@ldaws.com>
2022-02-08 13:11:55 +1100

    rust: move setup to setup script

---
+++ scripts/setup/common/executable_rust.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+crates=(
+  cargo-edit
+  cargo-outdated
+  cargo-udeps
+  cargo-update
+  evcxr_jupyter
+  evcxr_repl
+  jwt-cli
+)
+
+# can end up with rust on path through homebrew
+export RUSTUP_INIT_SKIP_PATH_CHECK="yes"
+
+if ! command -v rustup >/dev/null 2>&1; then
+  cd "$(mktemp -d)"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup-init
+  chmod +x rustup-init
+
+  ./rustup-init \
+    --no-modify-path \
+    --component rust-src \
+    -y
+
+  rm rustup-init
+fi
+
+if ! command -v cargo >/dev/null 2>&1; then
+  if test -x ~/.cargo/bin/cargo; then
+    export PATH="$HOME/.cargo/bin:$PATH"
+  else
+    echo "Couldn't find cargo on path, or in the default location. Exiting."
+    exit 127
+  fi
+fi
+
+for crate in "${crates[@]}"; do
+  cargo install "$crate"
+done
