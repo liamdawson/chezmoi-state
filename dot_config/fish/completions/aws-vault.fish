f0fa87a342941dfacf2a8c68c012ed2efb117220
Liam Dawson <liam@ldaws.com>
2022-06-21 11:33:35 +1000

    fish: improve aws-vault completions

--- dot_config/fish/completions/aws-vault.fish
+++ dot_config/fish/completions/aws-vault.fish
@@ -1,7 +1,7 @@
-if type -q aws-vault
+if status --is-interactive
   complete -ec aws-vault
 
-  # switch based on seeing a `--`
+  # switch based on seeing a standalone `--` token
   complete -c aws-vault -n 'not __fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_arg)'
   complete -c aws-vault -n '__fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_commandline)'
 
@@ -10,15 +10,16 @@ if type -q aws-vault
   end
 
   function __fish_aws_vault_complete_arg
-    set -l parts (commandline -opc)
+    # take the input command line, tokenize it into a list, and drop the first element (should be "aws-vault")
+    commandline -pc | read --local --tokenize --list parts
     set -e parts[1]
 
     aws-vault --completion-bash $parts
   end
 
   function __fish_aws_vault_complete_commandline
-    set -l parts (string split --max 1 '--' -- (commandline -pc))
+    set cmd_part (string replace --regex '^.*--\B' '' -- (commandline -pc))
 
-    complete "-C$parts[2]"
+    complete "-C$cmd_part"
   end
 end
