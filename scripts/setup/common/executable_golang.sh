a619fa3673c08858a68552a59abc49bc915e26cb
Liam Dawson <liam@ldaws.com>
2022-08-04 09:10:19 +1000

    common: experiment with go-httpbin

--- scripts/setup/common/executable_golang.sh
+++ scripts/setup/common/executable_golang.sh
@@ -5,6 +5,7 @@ set -Eeuo pipefail
 golang_packages=(
   golang.org/x/tools/cmd/godoc@latest
   github.com/brancz/gojsontoyaml@latest
+	github.com/mccutchen/go-httpbin/v2/cmd/go-httpbin@latest
 )
 
 for package in "${golang_packages[@]}"; do
