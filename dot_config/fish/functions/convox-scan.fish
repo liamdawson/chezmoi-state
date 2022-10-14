e3ee8c2c7eb5e0130ac544f9166daa4473cbdd51
Liam Dawson <liam@ldaws.com>
2022-10-14 13:07:51 +1100

    work: update convox-scan for 2fa prompt

--- dot_config/fish/functions/convox-scan.fish
+++ dot_config/fish/functions/convox-scan.fish
@@ -1,20 +1,22 @@
 if type -q convox
-  function convox-scan
-    set -l cache_dir "$HOME/.local/share/convox-cache"
-    mkdir -p $cache_dir
-    pushd $cache_dir || exit 1
+    function convox-scan
+        set -l cache_dir "$HOME/.local/share/convox-cache"
+        mkdir -p $cache_dir
+        pushd $cache_dir || return 1
 
-    # get racks
-    echo "Getting racks..."
+        # get racks
+        echo "Getting racks..."
 
-    convox racks | awk 'NR>1 { print $1; }' > racks
+        # in case of 2fa prompt
+        convox racks; or return $status
+        convox racks | awk 'NR>1 { print $1; }' >racks
 
-    printf "rack\\tapp\\n" > apps.tsv
-    for rack in (cat racks)
-      echo "Getting apps from $rack..."
-      convox apps --rack="$rack" | awk "NR>1 { printf \"%s\\t%s\\n\", \"$rack\", \$1; }" >> apps.tsv
+        printf "rack\\tapp\\n" >apps.tsv
+        for rack in (cat racks)
+            echo "Getting apps from $rack..."
+            convox apps --rack="$rack" | awk "NR>1 { printf \"%s\\t%s\\n\", \"$rack\", \$1; }" >>apps.tsv
+        end
+
+        popd
     end
-
-    popd
-  end
 end
