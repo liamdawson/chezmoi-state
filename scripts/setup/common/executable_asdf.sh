6ac7d15eeb334f99a63859f4b3c99ef04a04a9af
Liam Dawson <liam@ldaws.com>
2023-01-17 10:03:37 +1100

    common: use asdf postgres

--- scripts/setup/common/executable_asdf.sh
+++ scripts/setup/common/executable_asdf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-asdf plugin add nodejs
-asdf plugin add yarn
-asdf plugin add ruby
-asdf plugin add python
+asdf plugin-add nodejs
+asdf plugin-add yarn
+asdf plugin-add ruby
+asdf plugin-add python
+asdf plugin-add postgres
