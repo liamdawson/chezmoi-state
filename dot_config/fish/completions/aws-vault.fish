3ab1c7226e55cccb961cf45871013982883ad49e
Liam Dawson <liam@ldaws.com>
2023-06-06 10:21:04 +1000

    fish: fix aws-vault exec flag completions

--- dot_config/fish/completions/aws-vault.fish
+++ dot_config/fish/completions/aws-vault.fish
@@ -1,25 +1,25 @@
 if status --is-interactive
-  complete -ec aws-vault
+    complete -ec aws-vault
 
-  # switch based on seeing a standalone `--` token
-  complete -c aws-vault -n 'not __fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_arg)'
-  complete -c aws-vault -n '__fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_commandline)'
+    # switch based on seeing a standalone `--` token
+    complete -c aws-vault -n 'not __fish_aws_vault_is_commandline' -xa '(__fish_aws_vault_complete_arg)'
+    complete -c aws-vault -n __fish_aws_vault_is_commandline -xa '(__fish_aws_vault_complete_commandline)'
 
-  function __fish_aws_vault_is_commandline
-    string match -q -r '^--$' -- (commandline -opc)
-  end
+    function __fish_aws_vault_is_commandline
+        string match -q -r '^--$' -- (commandline -opc)
+    end
 
-  function __fish_aws_vault_complete_arg
-    # take the input command line, tokenize it into a list, and drop the first element (should be "aws-vault")
-    commandline -pc | read --local --tokenize --list parts
-    set -e parts[1]
+    function __fish_aws_vault_complete_arg
+        # take the input command line, tokenize it into a list, and drop the first element (should be "aws-vault")
+        commandline -pc | read --local --tokenize --list parts
+        set -e parts[1]
 
-    aws-vault --completion-bash $parts
-  end
+        aws-vault --completion-bash $parts
+    end
 
-  function __fish_aws_vault_complete_commandline
-    set cmd_part (string replace --regex '^.*--\B' '' -- (commandline -pc))
+    function __fish_aws_vault_complete_commandline
+        set cmd_part (string replace --regex '^.*?--\B' '' -- (commandline -pc))
 
-    complete "-C$cmd_part"
-  end
+        complete "-C$cmd_part"
+    end
 end
