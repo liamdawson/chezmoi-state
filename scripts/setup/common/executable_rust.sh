e59ce38c0f5d693ff341c5f0ce1f385d8279ebad
Liam Dawson <liam@ldaws.com>
2023-06-26 10:29:47 +1000

    rust: Install cargo-script-mvs

--- scripts/setup/common/executable_rust.sh
+++ scripts/setup/common/executable_rust.sh
@@ -3,52 +3,53 @@
 set -Eeuo pipefail
 
 crates=(
-	cross
 	cargo-cache
-  cargo-edit
-  cargo-outdated
-  cargo-udeps
-  cargo-update
-  evcxr_jupyter
-  evcxr_repl
-  https-cert-info
-  jwt-cli
+	cargo-script-mvs
+	cross
+	cargo-edit
+	cargo-outdated
+	cargo-udeps
+	cargo-update
+	evcxr_jupyter
+	evcxr_repl
+	https-cert-info
+	jwt-cli
 )
 
 configure_crate_updates() {
-  cargo install-update-config flowistry_ide --toolchain nightly
+	cargo install-update-config flowistry_ide --toolchain nightly
 }
 
 # can end up with rust on path through homebrew
 export RUSTUP_INIT_SKIP_PATH_CHECK="yes"
 
 if ! command -v rustup >/dev/null 2>&1; then
-  cd "$(mktemp -d)"
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >rustup-init
-  chmod +x rustup-init
+	cd "$(mktemp -d)"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >rustup-init
+	chmod +x rustup-init
 
-  ./rustup-init \
-    --no-modify-path \
-    --component rust-src \
-    -y
+	./rustup-init \
+		--no-modify-path \
+		--component rust-src \
+		-y
 
-  rm rustup-init
+	rm rustup-init
 
-  export PATH="$HOME/.cargo/bin:$PATH"
+	export PATH="$HOME/.cargo/bin:$PATH"
 
-  rustup toolchain install nightly \
-    --component rust-src \
-    --component rustc-dev \
-    --component llvm-tools-preview
+	rustup toolchain install nightly \
+		--component rust-src \
+		--component rustc-dev \
+		--component llvm-tools-preview
 fi
 
 if ! command -v cargo >/dev/null 2>&1; then
-  if test -x ~/.cargo/bin/cargo; then
-    export PATH="$HOME/.cargo/bin:$PATH"
-  else
-    echo "Couldn't find cargo on path, or in the default location. Exiting."
-    exit 127
-  fi
+	if test -x ~/.cargo/bin/cargo; then
+		export PATH="$HOME/.cargo/bin:$PATH"
+	else
+		echo "Couldn't find cargo on path, or in the default location. Exiting."
+		exit 127
+	fi
 fi
 
 if ! command -v cargo-binstall >/dev/null 2>&1; then
@@ -58,7 +59,7 @@ if ! command -v cargo-binstall >/dev/null 2>&1; then
 fi
 
 for crate in "${crates[@]}"; do
-  cargo binstall --no-confirm "$crate"
+	cargo binstall --no-confirm "$crate"
 done
 
 configure_crate_updates
