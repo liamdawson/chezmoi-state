33ea2e3ecf9513a0e8bd163f4bf79e100c28e1ce
Liam Dawson <liam@ldaws.com>
2023-10-24 12:00:19 +1100

    common: reconfigure apprise and install via brew

--- scripts/setup/common/executable_utils.sh
+++ scripts/setup/common/executable_utils.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
 main() {
-	install_apprise
-	install_doing
-}
-
-install_apprise() {
-	if ! command -v apprise >/dev/null 2>&1; then
-		command -v pipx >/dev/null 2>&1 || { echo "** apprise requires pipx, skipping install **"; return; }
-
-		pipx install apprise
-	fi
+	# install_doing
+	true
 }
 
 install_doing() {
