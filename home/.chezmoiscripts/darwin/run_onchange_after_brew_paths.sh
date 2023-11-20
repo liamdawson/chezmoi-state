9e0b19d9b162ab384db658416d04e230bcb1fbcd
Liam Dawson <liam@ldaws.com>
2023-11-20 21:02:21 +1100

    Some initial fish shell setup

---
+++ home/.chezmoiscripts/darwin/run_onchange_after_brew_paths.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+test -x /opt/homebrew/bin/brew || exit 0
+
+if ! [[ -f /etc/paths.d/brew-bin ]] || ! [[ -f /etc/paths.d/brew-sbin ]]; then
+	echo "Setting up Apple Silicon brew PATHs"
+	echo "/opt/homebrew/bin" | sudo tee /etc/paths.d/brew-bin >/dev/null
+	echo "/opt/homebrew/sbin" | sudo tee /etc/paths.d/brew-sbin >/dev/null
+fi
+
+if ! [[ -f /etc/manpaths.d/brew ]]; then
+	echo "Setting up Apple Silicon brew man paths"
+	echo "/opt/homebrew/share/man" | sudo tee /etc/manpaths.d/brew >/dev/null
+fi
