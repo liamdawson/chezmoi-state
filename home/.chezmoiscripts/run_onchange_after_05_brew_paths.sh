1bf215e633bdaeb225932a7de3e80ad20a36f7e3
Liam Dawson <liam@ldaws.com>
2023-12-01 15:20:39 +1100

    Move scripts around again

--- home/.chezmoiscripts/run_onchange_after_05_brew_paths.sh
+++ home/.chezmoiscripts/run_onchange_after_05_brew_paths.sh
@@ -5,7 +5,7 @@ set -Eeuo pipefail
 test -x /opt/homebrew/bin/brew || exit 0
 
 if ! [[ -f /etc/paths.d/brew-bin ]] || ! [[ -f /etc/paths.d/brew-sbin ]]; then
-	echo "Setting up Apple Silicon brew PATHs"
+	echo "Setting up Apple Silicon brew fallback PATHs"
 	echo "/opt/homebrew/bin" | sudo tee /etc/paths.d/brew-bin >/dev/null
 	echo "/opt/homebrew/sbin" | sudo tee /etc/paths.d/brew-sbin >/dev/null
 fi
