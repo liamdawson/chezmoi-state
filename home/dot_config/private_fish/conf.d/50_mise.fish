8dc7f45f2907643dc73e97641c6f58ed3c12a0e4
Liam Dawson <liam@ldaws.com>
2024-01-29 15:02:18 +1100

    Prepend mise shims

--- home/dot_config/private_fish/conf.d/50_mise.fish
+++ home/dot_config/private_fish/conf.d/50_mise.fish
@@ -1,1 +1,7 @@
-mise activate fish | source
+if type -q mise
+    if test -d "$HOME/.local/share/mise/shims"
+        fish_add_path --move --prepend --path "$HOME/.local/share/mise/shims"
+    end
+
+    mise activate fish | source
+end
