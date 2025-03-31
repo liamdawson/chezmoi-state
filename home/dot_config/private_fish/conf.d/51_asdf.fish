1692b0cc38a3fc3fc1c9ba40d8206131ea52c454
Liam Dawson <liam@ldaws.com>
2025-03-31 12:09:48 +1100

    asdf config improvements

--- home/dot_config/private_fish/conf.d/51_asdf.fish
+++ home/dot_config/private_fish/conf.d/51_asdf.fish
@@ -1,4 +1,15 @@
 # ASDF configuration code
-if test -e "$HOME/.asdf/asdf.fish"
-    source "$HOME/.asdf/asdf.fish"
+if command -v asdf >/dev/null 2>&1
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
+    end
+
+    # Do not use fish_add_path (added in Fish 3.2) because it
+    # potentially changes the order of items in PATH
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+    set --erase _asdf_shims
 end
