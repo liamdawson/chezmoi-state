3fda580fc14d869d8e5e91a30de27e6096d05608
Liam Dawson <liam@ldaws.com>
2025-04-14 15:54:18 +1000

    Find cargo directly after initial install

--- home/.chezmoiscripts/run_onchange_after_55_rust_pkgs.sh
+++ home/.chezmoiscripts/run_onchange_after_55_rust_pkgs.sh
@@ -2,17 +2,22 @@
 
 set -euo pipefail
 
+if ! command -v cargo >/dev/null 2>&1 && test -r "${HOME}/.cargo/env"; then
+  # shellcheck disable=SC1091
+  . "${HOME}/.cargo/env"
+fi
+
 if ! command -v cargo-binstall >/dev/null 2>&1; then
-    cargo install cargo-binstall
+  cargo install cargo-binstall
 fi
 
 packages=(
-    cargo-cache
-    cargo-outdated
-    cargo-update
-    cargo-watch
-    evcxr_repl
-    trunk
+  cargo-cache
+  cargo-outdated
+  cargo-update
+  cargo-watch
+  evcxr_repl
+  trunk
 )
 
 cargo binstall \
