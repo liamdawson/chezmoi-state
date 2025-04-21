e9e8e97ff5a5581330710d099bf5c5c34efdf409
Liam Dawson <liam@ldaws.com>
2025-04-22 09:51:49 +1000

    Update mise completions automatically

--- home/.chezmoiscripts/run_after_99_update_fish_completions.sh
+++ home/.chezmoiscripts/run_after_99_update_fish_completions.sh
@@ -3,3 +3,7 @@
 if command -v asdf >/dev/null 2>&1; then
   asdf completion fish >~/.config/fish/completions/asdf.fish
 fi
+
+if command -v mise >/dev/null 2>&1; then
+  mise completion fish > ~/.config/fish/completions/mise.fish
+fi
