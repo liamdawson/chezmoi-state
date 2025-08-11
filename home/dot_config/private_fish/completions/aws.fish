a420d841898012b7e0305a2c1ebe5c80b4ac5dc2
Liam Dawson <liam@ldaws.com>
2025-08-11 12:34:37 +1000

    Fix(?) AWS CLI completions

--- home/dot_config/private_fish/completions/aws.fish
+++ home/dot_config/private_fish/completions/aws.fish
@@ -1,8 +1,16 @@
 function __aws_complete
     set -lx COMP_SHELL fish
-    set -lx COMP_LINE (commandline -pc)
+    set -lx COMP_LINE (commandline -pcx) " "
+
+    set -l incomplete_token (commandline -pt)
+    set -l complete_tokens (commandline -pcx)
+
+    if string match -q -- "-*" (commandline -pt)
+        set COMP_LINE $COMP_LINE -
+    end
 
     aws_completer
 end
 
+complete -ec aws
 complete --command aws --no-files --arguments '(__aws_complete)'
