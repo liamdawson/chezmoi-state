1d4c2ccba4be56803f067765baa9c9a421957f96
Liam Dawson <liam@ldaws.com>
2025-09-12 10:21:34 +1000

    More colima config

--- scripts/darwin/setup/colima.sh
+++ scripts/darwin/setup/colima.sh
@@ -3,8 +3,23 @@
 set -euo pipefail
 
 main() {
-  colima start
+  if [ -f ~/.docker/config.json ]; then
+    if ! grep -qe '"/opt/homebrew/lib/docker/cli-plugins"' ~/.docker/config.json; then
+      cp ~/.docker/config.json ~/.docker/old.config.json 
+
+      cat ~/.docker/old.config.json | jq '. + {cliPluginsExtraDirs: .cliPluginsExtraDirs + ["/opt/homebrew/lib/docker/cli-plugins"]}' > ~/.docker/config.json 
+    fi
+  else
+    echo '{"cliPluginsExtraDirs": ["/opt/homebrew/lib/docker/cli-plugins"]}' > ~/.docker/config.json
+  fi
+
+  softwareupdate --install-rosetta --agree
+
+  env COLIMA_HOME="${HOME}/.colima" colima start --template
+  colima stop
+
   brew services start colima
 }
 
 main "$@"
+
