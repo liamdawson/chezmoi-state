a0f173e4003920e9d543440217aa70c9141eb399
Liam Dawson <liam@ldaws.com>
2025-09-15 14:18:03 +1000

    Workflow improvements

--- home/dot_config/private_fish/conf.d/90_abbreviations.fish
+++
@@ -1,1 +0,0 @@
-abbr -a bex bundle exec
---
+++ home/dot_config/private_fish/functions/wsidea.fish
@@ -0,0 +1,9 @@
+function wsidea --description "Open a workspace folder using IntelliJ IDEA"
+    set -l parts (string split --max 1 '--' -- "$argv")
+    set -l query $parts[1]
+
+    ws "$query" -- idea .
+end
+
+complete -ec wsidea
+complete -c wsidea -xa '(ghq list 2>/dev/null)'
