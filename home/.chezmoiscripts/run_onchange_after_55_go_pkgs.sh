50613ffb6f47ea7f6ac23f0a13ca733b1e298320
Liam Dawson <liam@ldaws.com>
2025-10-20 10:15:44 +1100

    Install go-lambda-runner

--- home/.chezmoiscripts/run_onchange_after_55_go_pkgs.sh
+++ home/.chezmoiscripts/run_onchange_after_55_go_pkgs.sh
@@ -6,6 +6,7 @@ export GOPATH="$HOME/src/go"
 
 packages=(
   github.com/nao1215/gup@latest
+  github.com/okzk/go-lambda-runner@latest
   golang.org/x/tools/cmd/godoc@latest
   golang.org/x/tools/cmd/goimports@latest
 )
