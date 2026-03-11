b833dcd4f06557f56d1dcbc830930e05f825f87f
Liam Dawson <liam@ldaws.com>
2026-03-11 16:03:52 +1100

    gca alias

--- home/dot_config/private_fish/conf.d/90_abbreviations.fish
+++ home/dot_config/private_fish/conf.d/90_abbreviations.fish
@@ -3,6 +3,8 @@ abbr --add grhh -- "git reset --hard HEAD"
 abbr --add grhoh -- "git reset --hard origin/HEAD"
 abbr --add grboh -- "git rebase origin/HEAD"
 
+abbr --add gca -- "git commit --amend"
+
 abbr --add gpf -- "git push --force-with-lease"
 abbr --add gallc -- "git add (git rev-parse --show-toplevel) && git commit"
 abbr --add --position anywhere gitroot -- "git rev-parse --show-toplevel"
