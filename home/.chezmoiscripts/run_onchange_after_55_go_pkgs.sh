3f741f33982309283db752f089afb0c05e10be26
Liam Dawson <liam@ldaws.com>
2026-03-16 10:48:08 +1100

    trial goconvey

--- home/.chezmoiscripts/run_onchange_after_55_go_pkgs.sh
+++ home/.chezmoiscripts/run_onchange_after_55_go_pkgs.sh
@@ -8,6 +8,7 @@ packages=(
   github.com/air-verse/air@latest
   github.com/nao1215/gup@latest
   github.com/okzk/go-lambda-runner@latest
+  github.com/smartystreets/goconvey@latest
   golang.org/x/perf/cmd/benchstat@latest
   golang.org/x/pkgsite/cmd/pkgsite@latest
   golang.org/x/tools/cmd/goimports@latest
