9f35ac71fa49b7c21257540a9d27ad42fec13b5b
Liam Dawson <liam@ldaws.com>
2026-03-17 15:37:51 +1100

    fix git aliases

--- home/dot_config/private_fish/conf.d/90_abbreviations.fish
+++ home/dot_config/private_fish/conf.d/90_abbreviations.fish
@@ -1,7 +1,9 @@
-abbr --add gfrboh -- "git fetch && git reset --hard origin/HEAD"
+abbr --add gfrboh -- "git fetch && git rebase origin/HEAD"
+abbr --add gfrhh -- "git fetch && git reset --hard HEAD"
+abbr --add gfrhoh -- "git fetch && git reset --hard origin/HEAD"
+abbr --add grboh -- "git rebase origin/HEAD"
 abbr --add grhh -- "git reset --hard HEAD"
 abbr --add grhoh -- "git reset --hard origin/HEAD"
-abbr --add grboh -- "git rebase origin/HEAD"
 
 abbr --add gca -- "git commit --amend"
 
