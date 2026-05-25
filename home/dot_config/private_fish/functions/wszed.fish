93473b50086854d83404343b6c173c55c35ecacd
Liam Dawson <liam@ldaws.com>
2026-05-25 10:10:53 +1000

    add wszed

---
+++ home/dot_config/private_fish/functions/wszed.fish
@@ -0,0 +1,9 @@
+function wszed --description "Open a workspace folder using Zed"
+    set -l parts (string split --max 1 '--' -- "$argv")
+    set -l query $parts[1]
+
+    ws "$query" -- zed .
+end
+
+complete -ec wszed
+complete -c wszed -xa '(ghq list 2>/dev/null)'
